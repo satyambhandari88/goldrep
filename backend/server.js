ac6462cbd204e25580e328229abb02ab5690d326
satyambhandari88 <satyambhandari7247@gmail.com>
2025-02-23 02:26:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,12 +45,15 @@ app.use("/api/preorders", preorderRoutes);
 app.use("/api/udhaar", udhaarRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 5000;
