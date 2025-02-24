b758c0f4d29f03e21adefb607aeed593151b8c9f
satyambhandari88 <satyambhandari7247@gmail.com>
2025-02-25 04:33:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Inventory from './components/Inventory';
